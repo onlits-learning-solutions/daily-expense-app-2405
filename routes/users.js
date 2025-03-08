eb0d168c912a61965e3567e01cd54dd7b492f152
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2025-03-08 18:37:17 +0530

    Create signup api Fixes #3

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const userController = require('../controllers/userController');
 /* GET users listing. */
 router.get('/', userController.getUsers);
+router.get('/:email', userController.getUser);
 
 /* POST users listing */
 router.post('/', userController.postUser);
