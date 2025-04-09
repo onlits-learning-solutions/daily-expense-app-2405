44600afb89ead359a83a8e8a19b02ba61cfb5fd8
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2025-04-09 17:45:47 +0530

    Create api to authenticate users Fixes #5

--- routes/users.js
+++ routes/users.js
@@ -8,5 +8,6 @@ router.get('/:email', userController.getUser);
 
 /* POST users listing */
 router.post('/', userController.postUser);
+router.post('/authenticate', userController.authenticate);
 
 module.exports = router;
